d53d456fe730af945c5babee1d4a0bd0751d8799
JJong <whdtls3878@gmail.com>
2020-05-15 14:00:59 +0900

    회원 가입 로직
    -이메일, 이름, 비밀번호가 조건에 맞게 들어갔는지 검사
    -회원 가입 API를 호출
    -가입 실패 혹은 성공 메시지를 경우에 알맞게 출력
    -성공 시에는 로그인 화면으로 전환

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/signup/SignUpViewModel.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/signup/SignUpViewModel.kt
@@ -2,11 +2,62 @@ package com.jjjoonngg.parayo.signup
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import com.jjjoonngg.parayo.api.ParayoApi
+import com.jjjoonngg.parayo.api.request.SignUpRequest
+import com.jjjoonngg.parayo.api.response.ApiResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
+import org.jetbrains.anko.error
 
 class SignUpViewModel(app: Application) : BaseViewModel(app) {
 
     val email = MutableLiveData("")
     val name = MutableLiveData("")
     val password = MutableLiveData("")
+
+    suspend fun signUp() {
+        val request = SignUpRequest(email.value, password.value, name.value)
+        if (isNotValidSignUp(request))
+            return
+
+        try {
+            val response = requestSignUp(request)
+            onSignUpResponse(response)
+        } catch (exception: Exception) {
+            error("SignUp Error", exception)
+            toast("알 수 없는 오류가 발생했습니다.")
+        }
+    }
+
+    private fun isNotValidSignUp(signUpRequest: SignUpRequest) =
+        when {
+            signUpRequest.isNotValidEmail() -> {
+                toast("이메일 형식이 정확하지 않습니다.")
+                true
+            }
+            signUpRequest.isNotValidPassword() -> {
+                toast("비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+                true
+            }
+            signUpRequest.isNotValidName() -> {
+                toast("이름은 2자 이상 20자 이하로 입력해주세요.")
+                true
+            }
+            else -> false
+        }
+
+    private suspend fun requestSignUp(request: SignUpRequest) =
+        withContext(Dispatchers.IO) {
+            ParayoApi.instance.signUp(request)
+        }
+
+    private fun onSignUpResponse(response: ApiResponse<Void>) {
+        if (response.success) {
+            toast("회원 가입이 되었습니다. 로그인 후 이용해주세요.")
+            finishActivity()
+        } else {
+            toast(response.message ?: "알 수 없는 오류가 발생했습니다.")
+        }
+    }
 }
