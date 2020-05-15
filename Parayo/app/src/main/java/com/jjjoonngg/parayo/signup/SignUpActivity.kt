b2a2b555977b8f60652424b0f7e4855b3bc28491
JJong <whdtls3878@gmail.com>
2020-05-15 15:14:34 +0900

    UI 와 Activity 연동

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/signup/SignUpActivity.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/signup/SignUpActivity.kt
@@ -1,7 +1,17 @@
 package com.jjjoonngg.parayo.signup
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import net.codephobia.ankomvvm.components.BaseActivity
+import org.jetbrains.anko.setContentView
 
 class SignUpActivity : BaseActivity<SignUpViewModel>() {
     override val viewModelType = SignUpViewModel::class
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        SignUpActivityUI(getViewModel())
+            .setContentView(this)
+    }
 }
