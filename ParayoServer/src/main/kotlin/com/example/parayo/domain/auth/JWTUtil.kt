4c62b34c88897372c9d01b9878477ee2d35cb72e
JJong <whdtls3878@gmail.com>
2020-05-15 15:37:50 +0900

    리프레시용 토큰 생성 함수 추가

--- ParayoServer/src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
+++ ParayoServer/src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
@@ -9,9 +9,13 @@ object JWTUtil {
     private const val ISSUER = "Parayo"
     private const val SUBJECT = "Auth"
     private const val EXPIRE_TIME = 60L * 60 * 2 * 1000 // 2 hours
+    private const val REFRESH_EXPIRE_TIME = 60L * 60 * 24 * 30 * 1000 // 30 Days
 
     private val SECRET = "your-secret"
     private val algorithm: Algorithm = Algorithm.HMAC256(SECRET)
+
+    private val refreshSecret = "your-refresh-secret"
+    private val refreshAlgorithm: Algorithm = Algorithm.HMAC256(refreshSecret)
 
     fun createToken(email: String) = JWT.create()
         .withIssuer(ISSUER)
@@ -20,6 +24,14 @@ object JWTUtil {
         .withExpiresAt(Date(Date().time + EXPIRE_TIME))
         .withClaim(JWTClaims.EMAIL, email)
         .sign(algorithm)
+
+    fun createRefreshToken(email: String) = JWT.create()
+        .withIssuer(ISSUER)
+        .withSubject(SUBJECT)
+        .withIssuedAt(Date())
+        .withExpiresAt(Date(Date().time + REFRESH_EXPIRE_TIME))
+        .withClaim(JWTClaims.EMAIL, email)
+        .sign(refreshAlgorithm)
 
     object JWTClaims {
         const val EMAIL = "email"
