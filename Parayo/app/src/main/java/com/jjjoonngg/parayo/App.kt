b8d34df585645e7b36b4521891838fcd07bb28b5
JJong <whdtls3878@gmail.com>
2020-05-15 17:06:33 +0900

    Custom Application Class 작성

---
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/App.kt
@@ -0,0 +1,14 @@
+package com.jjjoonngg.parayo
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+    }
+}
