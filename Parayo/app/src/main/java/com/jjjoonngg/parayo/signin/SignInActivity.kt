7d72b2a7fa770b4eedc664b6dec98495e5db22ea
JJong <whdtls3878@gmail.com>
2020-05-15 16:28:02 +0900

    Signin Activity add

---
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/signin/SignInActivity.kt
@@ -0,0 +1,7 @@
+package com.jjjoonngg.parayo.signin
+
+import net.codephobia.ankomvvm.components.BaseActivity
+
+class SignInActivity : BaseActivity<SignInViewModel>() {
+    override val viewModelType = SignInViewModel::class
+}
