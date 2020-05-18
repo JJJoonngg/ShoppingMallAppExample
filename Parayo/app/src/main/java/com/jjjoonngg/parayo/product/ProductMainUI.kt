3cafb62354335c20122a49df8e198f2cf8982145
JJong <whdtls3878@gmail.com>
2020-05-18 20:43:05 +0900

    Navigation menu add
    -이미지 vector add
    -로그아웃 선택시 기능 구현
    -문의는 toast msg 만 출력

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainUI.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainUI.kt
@@ -1,11 +1,15 @@
 package com.jjjoonngg.parayo.product
 
 import android.view.Gravity
+import android.view.Menu.NONE
+import android.view.MenuItem
 import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.jjjoonngg.parayo.R
+import com.jjjoonngg.parayo.common.Prefs
+import com.jjjoonngg.parayo.signin.SignInActivity
 import com.jjjoonngg.parayo.view.borderBottom
 import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.toolbar
@@ -14,7 +18,8 @@ import org.jetbrains.anko.support.v4.drawerLayout
 
 class ProductMainUI(
     private val viewModel: ProductMainViewModel
-) : AnkoComponent<ProductMainActivity> {
+) : AnkoComponent<ProductMainActivity>,
+    NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
@@ -38,9 +43,41 @@ class ProductMainUI(
             navigationView = navigationView {
                 ProductMainNavHeader()
                     .createView(AnkoContext.create(context, this))
-                    .run(::addHeaderView)
+                    .let(::addHeaderView)
+
+                menu.apply {
+                    add(NONE, MENU_ID_INQUIRY, NONE, "내 문의").apply {
+                        setIcon(R.drawable.ic_chat)
+                    }
+                    add(NONE, MENU_ID_LOGOUT, NONE, "로그아웃").apply {
+                        setIcon(R.drawable.ic_signout)
+                    }
+                }
+
+                setNavigationItemSelectedListener(this@ProductMainUI)
             }.lparams(wrapContent, matchParent) {
                 gravity = Gravity.START
             }
         }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            MENU_ID_INQUIRY -> {
+                viewModel.toast("내 문의")
+            }
+            MENU_ID_LOGOUT -> {
+                Prefs.token = null
+                Prefs.refreshToken = null
+                viewModel.startActivityAndFinish<SignInActivity>()
+            }
+        }
+        drawerLayout.closeDrawer(navigationView)
+        return true
+    }
+
+
+    companion object {
+        private const val MENU_ID_INQUIRY = 1
+        private const val MENU_ID_LOGOUT = 2
+    }
 }
