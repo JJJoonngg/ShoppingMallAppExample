4a97689c2b20015a9d3225855d4face51470515f
JJong <whdtls3878@gmail.com>
2020-05-18 20:12:05 +0900

    add borderline to toolbar

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainUI.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainUI.kt
@@ -2,8 +2,10 @@ package com.jjjoonngg.parayo.product
 
 import android.view.Gravity
 import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
+import android.widget.Toolbar
 import com.google.android.material.navigation.NavigationView
 import com.jjjoonngg.parayo.R
+import com.jjjoonngg.parayo.view.borderBottom
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.navigationView
 import org.jetbrains.anko.support.v4.drawerLayout
@@ -13,12 +15,15 @@ class ProductMainUI(
 ) : AnkoComponent<ProductMainActivity> {
 
     lateinit var navigationView: NavigationView
+    lateinit var toolbar: Toolbar
 
     override fun createView(ui: AnkoContext<ProductMainActivity>) =
         ui.drawerLayout {
             verticalLayout {
-                toolbar {
+                toolbar = toolbar {
                     title = "Parayo"
+                    bottomPadding = dip(1)
+                    background = borderBottom(width = dip(1))
                     menu.add("Search")
                         .setIcon(R.drawable.ic_search_black)
                         .setShowAsAction(SHOW_AS_ACTION_ALWAYS)
