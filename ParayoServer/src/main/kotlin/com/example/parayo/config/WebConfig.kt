4a655c0eabd3b43620b927da2655fbe10ca774c6
JJong <whdtls3878@gmail.com>
2020-05-19 13:22:46 +0900

    인터셉터를 사용할 수 있게 등록 - WebConfig

---
+++ ParayoServer/src/main/kotlin/com/example/parayo/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package com.example.parayo.config
+
+import com.example.parayo.interceptor.TokenValidationInterceptor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig @Autowired constructor(
+    private val tokenValidationInterceptor: TokenValidationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(tokenValidationInterceptor)
+            .addPathPatterns("/api/**")
+    }
+}
