69ff04e07a8fdf9de94a378daa7cef8bec634c7d
JJong <whdtls3878@gmail.com>
2020-05-19 12:58:19 +0900

    JWT 토큰 검증 및 이메일 추출 코드 추가

--- ParayoServer/src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
+++ ParayoServer/src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
@@ -2,6 +2,7 @@ package com.example.parayo.domain.auth
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
 import java.util.*
 
 object JWTUtil {
@@ -32,6 +33,21 @@ object JWTUtil {
         .withExpiresAt(Date(Date().time + REFRESH_EXPIRE_TIME))
         .withClaim(JWTClaims.EMAIL, email)
         .sign(refreshAlgorithm)
+
+    fun verify(token: String): DecodedJWT =
+        JWT.require(algorithm)
+            .withIssuer(ISSUER)
+            .build()
+            .verify(token)
+
+    fun verifyRefresh(token: String): DecodedJWT =
+        JWT.require(refreshAlgorithm)
+            .withIssuer(ISSUER)
+            .build()
+            .verify(token)
+
+    fun extractEmail(jwt: DecodedJWT): String =
+        jwt.getClaim(JWTClaims.EMAIL).asString()
 
     object JWTClaims {
         const val EMAIL = "email"
