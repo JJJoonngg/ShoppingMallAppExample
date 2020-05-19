4527016dbeea503fd2ef4d9ee5181050b4745740
JJong <whdtls3878@gmail.com>
2020-05-19 13:26:11 +0900

    토큰 재발급 API 작성

--- ParayoServer/src/main/kotlin/com/example/parayo/controller/SignInApiController.kt
+++ ParayoServer/src/main/kotlin/com/example/parayo/controller/SignInApiController.kt
@@ -1,21 +1,36 @@
 package com.example.parayo.controller
 
 import com.example.parayo.common.ApiResponse
+import com.example.parayo.domain.auth.JWTUtil
 import com.example.parayo.domain.auth.SignInRequest
 import com.example.parayo.domain.auth.SignInService
+import com.example.parayo.domain.auth.UserContextHolder
+import com.example.parayo.interceptor.TokenValidationInterceptor
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.lang.IllegalArgumentException
 
 @RestController
 @RequestMapping("/api/v1")
 class SignInApiController @Autowired constructor(
-    private val signInService: SignInService
+    private val signInService: SignInService,
+    private val userContextHolder: UserContextHolder
 ) {
 
     @PostMapping("/signin")
     fun signIn(@RequestBody signInRequest: SignInRequest) =
         ApiResponse.ok(signInService.signIn(signInRequest))
+
+    @PostMapping("/refresh_token")
+    fun refreshToken(
+        @RequestParam("grant_type") grantType: String
+    ): ApiResponse {
+        if (grantType != TokenValidationInterceptor.GRANT_TYPE_REFRESH) {
+            throw IllegalArgumentException("grant_type 없음")
+        }
+
+        return userContextHolder.email?.let {
+            ApiResponse.ok(JWTUtil.createToken(it))
+        } ?: throw IllegalArgumentException("사용자 정보 없음")
+    }
 }
