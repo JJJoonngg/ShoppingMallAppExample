7696d1d3168697c7fba5833fef952959f7aa0538
JJong <whdtls3878@gmail.com>
2020-05-19 15:38:27 +0900

    상품 등록 로직 작성 및 컨트롤러에 연결

--- ParayoServer/src/main/kotlin/com/example/parayo/controller/ProductApiController.kt
+++ ParayoServer/src/main/kotlin/com/example/parayo/controller/ProductApiController.kt
@@ -2,8 +2,11 @@ package com.example.parayo.controller
 
 import com.example.parayo.common.ApiResponse
 import com.example.parayo.domain.product.registration.ProductImageService
+import com.example.parayo.domain.product.registration.ProductRegistrationRequest
+import com.example.parayo.domain.product.registration.ProductRegistrationService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
@@ -11,11 +14,19 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 @RequestMapping("/api/v1")
 class ProductApiController @Autowired constructor(
-    private val productImageService: ProductImageService
+    private val productImageService: ProductImageService,
+    private val productRegistration: ProductRegistrationService
 ) {
 
     @PostMapping("/product_images")
     fun uploadImage(image: MultipartFile) = ApiResponse.ok(
         productImageService.uploadImage(image)
+    )
+
+    @PostMapping("/products")
+    fun register(
+        @RequestBody request: ProductRegistrationRequest
+    ) = ApiResponse.ok(
+        productRegistration.register(request)
     )
 }
