837468ed955f97b403dfebc44e99e742b21bbc92
JJong <whdtls3878@gmail.com>
2020-05-19 15:25:46 +0900

    DB로 부터 정보 읽어오기 위해 함수 추가

--- ParayoServer/src/main/kotlin/com/example/parayo/domain/product/ProductImageRepository.kt
+++ ParayoServer/src/main/kotlin/com/example/parayo/domain/product/ProductImageRepository.kt
@@ -2,4 +2,6 @@ package com.example.parayo.domain.product
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProductImageRepository : JpaRepository<ProductImage, Long> {}
+interface ProductImageRepository : JpaRepository<ProductImage, Long> {
+    fun findByIdIn(imageIds: List<Long>): MutableList<ProductImage>
+}
