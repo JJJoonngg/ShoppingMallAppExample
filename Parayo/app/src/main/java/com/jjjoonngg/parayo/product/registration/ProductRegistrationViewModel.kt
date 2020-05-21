3a0f1135a706f2424642ebf38a7792ca245fb9fb
JJong <whdtls3878@gmail.com>
2020-05-21 18:55:38 +0900

    ProductImageUploader를 이용한 이미지 등록 로직 구현

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/product/registration/ProductRegistrationViewModel.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/registration/ProductRegistrationViewModel.kt
@@ -1,8 +1,14 @@
 package com.jjjoonngg.parayo.product.registration
 
+import android.app.Activity.RESULT_OK
 import android.app.Application
+import android.content.Intent
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.jjjoonngg.parayo.api.response.ApiResponse
+import com.jjjoonngg.parayo.api.response.ProductImageUploadResponse
 import com.jjjoonngg.parayo.product.category.categoryList
+import kotlinx.coroutines.launch
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
 
 class ProductRegistrationViewModel(app: Application) : BaseViewModel(app) {
@@ -29,6 +35,8 @@ class ProductRegistrationViewModel(app: Application) : BaseViewModel(app) {
     val productNameLength = MutableLiveData("0/$productNameLimit")
     val descriptionLength = MutableLiveData("0/$descriptionLimit")
 
+    var currentImageNum = 0
+
     fun checkProductNameLength() {
         productName.value?.let {
             if (it.length > productNameLimit) {
@@ -49,5 +57,41 @@ class ProductRegistrationViewModel(app: Application) : BaseViewModel(app) {
 
     fun onCategorySelect(position: Int) {
         categoryIdSelected = categoryList[position].id
+    }
+
+    fun pickImage(imageNum: Int) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply { type = "image/*" }
+        intent.resolveActivity(app.packageManager)?.let {
+            startActivityForResult(intent, REQUEST_PICK_IMAGES)
+        }
+        currentImageNum = imageNum
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_OK) return
+        when (requestCode) {
+            REQUEST_PICK_IMAGES -> data?.let { uploadImage(it) }
+        }
+    }
+
+    private fun uploadImage(intent: Intent) =
+        getContent(intent.data)?.let { imageFile ->
+            viewModelScope.launch {
+                val response = ProductImageUploader().upload(imageFile)
+                onImageUploadResponse(response)
+            }
+        }
+
+    private fun onImageUploadResponse(response: ApiResponse<ProductImageUploadResponse>) {
+        if (response.success && response.data != null) {
+            imagesUrls[currentImageNum].value = response.data.filePath
+            imagesIds[currentImageNum] = response.data.productImageId
+        } else {
+            toast(response.message ?: "알 수 없는 오류가 발생했습니다.")
+        }
+    }
+
+    companion object {
+        const val REQUEST_PICK_IMAGES = 0
     }
 }
