af214aacc0d312b779d293f1ac416007b298276a
JJong <whdtls3878@gmail.com>
2020-05-26 23:13:55 +0900

    viewPager 내부에 들어갈 Fragment 작성

---
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/list/ProductListFragment.kt
@@ -0,0 +1,46 @@
+package com.jjjoonngg.parayo.product.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import net.codephobia.ankomvvm.components.BaseFragment
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.textView
+import org.jetbrains.anko.verticalLayout
+
+class ProductListFragment : BaseFragment<ProductListViewModel>() {
+
+    override val viewModelType = ProductListViewModel::class
+
+    val categoryId
+        get() = arguments?.getInt("categoryId")
+            ?: throw  IllegalStateException("categoryId 없음")
+    val title
+        get() = arguments?.getString("title")
+            ?: throw  IllegalStateException("title 없음")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return AnkoContext.create(ctx, this).verticalLayout {
+            textView(categoryId.toString())
+            textView(title)
+        }
+    }
+
+    companion object {
+        fun newInstance(categoryId: Int, title: String) =
+            ProductListFragment().apply {
+                arguments = Bundle().also {
+                    it.putInt("categoryId", categoryId)
+                    it.putString("title", title)
+                }
+            }
+    }
+
+
+}
