a65220b3b9afa588fc6f61f83c77ff2b7fddf2f4
JJong <whdtls3878@gmail.com>
2020-05-26 23:18:28 +0900

    TabLayout 과 ViewPager 연결코드 작성

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainActivity.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainActivity.kt
@@ -3,6 +3,7 @@ package com.jjjoonngg.parayo.product
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.jjjoonngg.parayo.R
+import com.jjjoonngg.parayo.product.list.ProductListPagerAdapter
 import net.codephobia.ankomvvm.components.BaseActivity
 import org.jetbrains.anko.setContentView
 
@@ -14,6 +15,9 @@ class ProductMainActivity : BaseActivity<ProductMainViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui.setContentView(this)
+        ui.viewPager.adapter =
+            ProductListPagerAdapter(supportFragmentManager)
+        ui.tablayout.setupWithViewPager(ui.viewPager)
         setupDrawerListener()
     }
 
