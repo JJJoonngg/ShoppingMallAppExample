a3ea6dd4801b685f09a92f63cc834d81acf3cc0e
JJong <whdtls3878@gmail.com>
2020-05-26 23:03:56 +0900

    상품 리스트의 탭과 viewPager 추가

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainUI.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/ProductMainUI.kt
@@ -4,9 +4,15 @@ import android.view.Gravity
 import android.view.Menu.NONE
 import android.view.MenuItem
 import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
+import android.view.View.generateViewId
+import android.widget.TableLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.GRAVITY_FILL
+import com.google.android.material.tabs.TabLayout.MODE_SCROLLABLE
 import com.jjjoonngg.parayo.R
 import com.jjjoonngg.parayo.common.Prefs
 import com.jjjoonngg.parayo.signin.SignInActivity
@@ -15,8 +21,11 @@ import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.toolbar
 import org.jetbrains.anko.design.floatingActionButton
 import org.jetbrains.anko.design.navigationView
+import org.jetbrains.anko.design.tabLayout
+import org.jetbrains.anko.design.themedTabLayout
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.support.v4.drawerLayout
+import org.jetbrains.anko.support.v4.viewPager
 
 class ProductMainUI(
     private val viewModel: ProductMainViewModel
@@ -25,6 +34,8 @@ class ProductMainUI(
 
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
+    lateinit var tablayout: TabLayout
+    lateinit var viewPager: ViewPager
     lateinit var toolbar: Toolbar
 
     override fun createView(ui: AnkoContext<ProductMainActivity>) =
@@ -41,7 +52,21 @@ class ProductMainUI(
                             .setIcon(R.drawable.ic_search_black)
                             .setShowAsAction(SHOW_AS_ACTION_ALWAYS)
                     }.lparams(matchParent, wrapContent)
-                }.lparams(matchParent, matchParent)
+
+                    tablayout = themedTabLayout(
+                        R.style.Widget_MaterialComponents_TabLayout
+                    ) {
+                        bottomPadding = dip(1)
+                        tabMode = MODE_SCROLLABLE
+                        tabGravity = GRAVITY_FILL
+                        background = borderBottom(width = dip(1))
+                        lparams(matchParent, wrapContent)
+                    }
+
+                    viewPager = viewPager {
+                        id = generateViewId()
+                    }.lparams(matchParent, matchParent)
+                }
 
                 floatingActionButton {
                     imageResource = R.drawable.ic_add
