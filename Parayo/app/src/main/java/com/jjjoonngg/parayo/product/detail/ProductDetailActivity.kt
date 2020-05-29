07870308b2c9b7b58f15a8ed7100f06c2f33ac34
JJong <whdtls3878@gmail.com>
2020-05-30 00:01:04 +0900

    상세화면 Activity 등록

---
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/detail/ProductDetailActivity.kt
@@ -0,0 +1,8 @@
+package com.jjjoonngg.parayo.product.detail
+
+import net.codephobia.ankomvvm.components.BaseActivity
+
+class ProductDetailActivity : BaseActivity<ProductDetailViewModel>() {
+
+    override val viewModelType = ProductDetailViewModel::class
+}
