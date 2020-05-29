22d93f6b7b779682497772626d68b36a3d3c7249
JJong <whdtls3878@gmail.com>
2020-05-30 00:30:00 +0900

    상세화면 Activity 앱바 사용 - 앱테마 설정-> 기본 앱바 사용

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/product/detail/ProductDetailActivity.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/product/detail/ProductDetailActivity.kt
@@ -1,8 +1,38 @@
 package com.jjjoonngg.parayo.product.detail
 
+import android.os.Bundle
+import android.view.MenuItem
 import net.codephobia.ankomvvm.components.BaseActivity
+import org.jetbrains.anko.setContentView
 
 class ProductDetailActivity : BaseActivity<ProductDetailViewModel>() {
 
     override val viewModelType = ProductDetailViewModel::class
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+
+        val viewModel = getViewModel()
+        val productId = intent.getLongExtra(PRODUCT_ID, -1)
+
+        viewModel.loadDetail(productId)
+        ProductDetailUI(getViewModel()).setContentView(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            when (item.itemId) {
+                android.R.id.home -> onBackPressed()
+                else -> {
+                }
+            }
+        }
+        return true
+    }
+
+    companion object {
+        val PRODUCT_ID = "productId"
+    }
 }
