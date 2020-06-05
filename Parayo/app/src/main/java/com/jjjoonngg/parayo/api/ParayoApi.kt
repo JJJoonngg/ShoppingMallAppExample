46780406e276ed0a06d66692724e2a34a2df0c18
JJong <whdtls3878@gmail.com>
2020-06-05 21:24:32 +0900

    토큰 API 추가

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/api/ParayoApi.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/api/ParayoApi.kt
@@ -48,4 +48,8 @@ interface ParayoApi {
     @GET("/api/v1/products/{id}")
     suspend fun getProduct(@Path("id") id: Long)
             : ApiResponse<ProductResponse>
+
+    @PUT("/api/v1/users/fcm-token")
+    suspend fun updateFcmToken(fcmToken: String)
+            : ApiResponse<Response<Void>>
 }
