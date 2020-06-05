960f45dd2c0aafcb21ed8f8b8c5aaf89a2e120f4
JJong <whdtls3878@gmail.com>
2020-06-05 21:44:37 +0900

    푸쉬알림 처리 함수 <onMessageReceived> override

--- Parayo/app/src/main/java/com/jjjoonngg/parayo/fcm/ParayoMessagingService.kt
+++ Parayo/app/src/main/java/com/jjjoonngg/parayo/fcm/ParayoMessagingService.kt
@@ -1,12 +1,18 @@
 package com.jjjoonngg.parayo.fcm
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.jjjoonngg.parayo.R
 import com.jjjoonngg.parayo.api.ParayoApi
 import com.jjjoonngg.parayo.common.Prefs
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.error
-import org.jetbrains.anko.warn
+import org.jetbrains.anko.*
 
 class ParayoMessagingService : FirebaseMessagingService(), AnkoLogger {
 
@@ -20,9 +26,40 @@ class ParayoMessagingService : FirebaseMessagingService(), AnkoLogger {
                         warn(response.message ?: "토큰 업데이트 실패")
                     }
                 } catch (exception: Exception) {
-                    error(exception.message ?: "토큰 업데티으 실패", exception)
+                    error(exception.message ?: "토큰 업데이트 실패", exception)
                 }
             }
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        message?.data?.let { data ->
+            debug(data)
+            createNotificationChannelIfNeeded()
+
+            val bulider = NotificationCompat
+                .Builder(this, "channel.parayo.default")
+                .setContentTitle(data["title"])
+                .setContentText(data["content"])
+                .setSmallIcon(R.drawable.ic_logo)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NotificationId.generate(), bulider.build())
+            }
+        }
+    }
+
+    private fun createNotificationChannelIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "channel.parayo.default",
+                "기본 알림",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "기본 알림"
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
